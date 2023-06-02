e369ea856ec0d5e2ae19b95b94cd355dbb4a903d
leojordano <leonardojordano1@gmail.com>
2023-06-02 12:24:01 -0300

    FIX: Adjust backgrund gradient

--- src/app/Components/Pages/Home/Styles.tsx
+++ src/app/Components/Pages/Home/Styles.tsx
@@ -4,8 +4,8 @@ const SContainer = styled.div`
   height: 100vh;
   border-radius: ${({theme}) => theme.radius};
   padding: 80px 24px 24px;
-  background: rgb(87,7,7);
-  background: linear-gradient(180deg, rgba(87,7,7,1) 0%, rgba(2,0,0,1) 47%, rgba(0,0,0,1) 100%);
+  background-color: ${({theme}) => theme.colors.lightGray};
+  background: linear-gradient(180deg, rgb(87, 7, 7) -20%, ${({theme}) => theme.colors.lightBlack} 60%,${({theme}) => theme.colors.lightBlack} 100%);
 `
 
 const SLastedActionsContainer = styled.div`
